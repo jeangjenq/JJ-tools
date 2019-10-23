82b57876ae81fad4488ec3efbb6b67d9a520d53c
jeangjenq <jeangjenq@hotmail.com>
2019-10-23 15:01:51 +1300

    propertiesToggle v2.0 Changed hotkeys and added function.
    	Added functionality of opening node's properties inside a group/gizmo.
        Toggle properties, ' \ '
        Open properties of node inside a group/gizmo, ' CTRL+\ '
        Close all properties, ' ALT+a '

--- propertiesToggle.py
+++ propertiesToggle.py
@@ -1,50 +1,86 @@
+version = 2.0
 '''
-propertiesToggle v1.0
+propertiesToggle
 
 Written by Jeang Jenq Loh
-22 March 2019
+27 April 2019
 ###########################
 
-showToggle(), ','
+showToggle(nodes), '\'
 Toggle node floating properties
 
-showConfirmation()
+showConfirmation(nodes)
 Confirmation if opening more than 10 properties at a time
+
+toggleInside(), 'Ctrl + \'
+Show properties of a node inside a group/gizmo
 
 close(), 'Alt + a'
 Close all opened properties, including project settings
-
 '''
 
 import nuke
+import nukescripts
+import webbrowser
 
-#function to toggle properties (as floating only)
-def showToggle():
-    for node in nuke.selectedNodes():
+
+# function to toggle properties (as floating only)
+def showToggle(nodes):
+    for node in nodes:
         if node.shown():
             node.hideControlPanel()
         else:
             node.showControlPanel(forceFloat=True)
 
-#confirm with user if opening more than 10 floating properties
-def showConfirmation():
+
+# confirm with user if opening more than 10 floating properties
+def showConfirmation(nodes):
     showing = 0
-    for node in nuke.selectedNodes():
+    for node in nodes:
         if not node.shown():
             showing += 1
 
-    if showing > 10:
+    if showing < 10:
+        showToggle(nodes)
+    else:
         confirmation = "This action will open " + str(showing) + ' control panels and may take a long time. Do you want to continue?'
         if nuke.ask(confirmation):
-            showToggle()
-    else:
-        showToggle()
+            showToggle(nodes)
 
-#close all properties and project setting if opened
+
+# function to toggle properties inside a group/gizmo
+def toggleInside():
+    # Create drop down python panel to select node
+    class toggleInsidePanel(nukescripts.PythonPanel):
+        def __init__(self):
+            nukescripts.PythonPanel.__init__(self, "Show properties of")
+            nodes = []
+            for node in nuke.allNodes(group=nuke.selectedNode()):
+                nodes.append(node.name())
+            self.nodeList = nuke.Enumeration_Knob('showNode', "Show node", nodes)
+            self.addKnob(self.nodeList)
+
+        def showModalDialog(self):
+            show = nukescripts.PythonPanel.showModalDialog(self)
+            if show:
+                nameInRoot = nuke.selectedNode().name() + "." + self.nodeList.value()
+                showToggle([nuke.toNode(nameInRoot)])
+
+    # If selected node is not a group/gizmo, show node's properties instead
+    if 'gizmo_file' in nuke.selectedNode().knobs() or nuke.selectedNode().Class() == 'Group':
+        toggleInsidePanel().showModalDialog()
+    else:
+        showConfirmation(nuke.selectedNodes())
+
+
+# close all properties and project setting if opened
 def close():
     nuke.root().hideControlPanel()
     [node.hideControlPanel() for node in nuke.allNodes(recurseGroups=True)]
 
-#Add to menu and assign shortcut key
-nuke.menu('Nuke').findItem('Edit').addCommand('Close all properties', 'closeProperties.close()', 'alt+a')
-nuke.menu('Nuke').findItem('Edit').addCommand('Toggle selected properties', 'closeProperties.showConfirmation()', ',')
+# Add to menu and assign shortcut key
+propertiesMenu = nuke.menu('Nuke').findItem('Edit/Node').addMenu('Properties')
+propertiesMenu.addCommand('propertiesToggle ' + str(version), "webbrowser.open('http://www.nukepedia.com/python/nodegraph/propertiestoggle')")
+propertiesMenu.addCommand('Close all properties', 'propertiesToggle.close()', 'alt+a')
+propertiesMenu.addCommand('Toggle selected properties', 'propertiesToggle.showConfirmation(nuke.selectedNodes())', '\\')
+propertiesMenu.addCommand('Show properties of node inside group\/gizmo', 'propertiesToggle.toggleInside()', 'ctrl+\\')
